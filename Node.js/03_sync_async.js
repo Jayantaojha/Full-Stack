d545dcc2c0e256035d3f50e40b781848f0b86aa2
Jayanta Ojha <95135086+Jayantaojha@users.noreply.github.com>
2023-12-19 12:50:29 +0000

    Async defination added.

--- Node.js/03_sync_async.js
+++ Node.js/03_sync_async.js
@@ -15,3 +15,9 @@ const text = fs.readFile("./Node.js/dele.txt", "utf-8" ,(err, data) => {
     console.log(data);
 })
 console.log("This is a message!");
+
+
+/*
+Notes:
+ a block of code where line by line execution is not guaranteed is known as asynchronous or non-blocking code. These types of codes accept a call-back function.
+*/
